5f20e6fcfeec4be0e829c67b3fa9673f7d244585
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2020-08-18 16:56:35 -0500

    add user view and form

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
+from django.contrib.auth import get_user_model
+
+from .forms import UserForm
 
 
 login_page = 'base.html'
 logout_page = ''
+register_page = ''
+
+
+User = get_user_model()
 
 
 class LoginView(DjangoLoginView):
@@ -13,4 +21,11 @@ class LoginView(DjangoLoginView):
 
 class LogoutView(DjangoLogoutView):
     template_name = logout_page
+
+
+class RegisterView(SuccessMessageMixin, CreateView):
+    template_name = register_page
+    model = User
+    form_class = UserForm
+    success_message = '%(email)s cadastrado com sucesso'
 
