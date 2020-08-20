f5bfac44bc81e3d60b5b7e28743fa93689266876
Starkus1999 <thalisson.bandeira@hotmail.com>
2020-08-20 11:08:35 -0500

    Front: melhores nas telas & Criado formulario.html

--- musicoterapia/urls.py
+++ musicoterapia/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from users.views import LoginView, LogoutView, RegisterView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('formulario/', TemplateView.as_view(template_name='formulario.html'), name='formulario'),
     path('teste', include('base.urls'))
 ]
